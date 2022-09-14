d70c2bacc60561f8c4cb3a638f7c8a8127f6cbec
Arnold Burnley <aburnleylvx@outlook.com>
2022-09-14 08:22:34 -0400

    fleshed out NavBar

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Lean4Real';
+  products: any[]
+  
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:7307/api/products').subscribe((response: any) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+  
 }
